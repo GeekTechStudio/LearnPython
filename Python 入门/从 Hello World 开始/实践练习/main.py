00fccd02f989cb38d176306d49a305fb2ba6ed90
韩翔宇 <admin@pescn.cn>
2022-08-29 19:16:10 +0800

    新增 - 从 Hello World 开始

---
+++ Python 入门/从 Hello World 开始/实践练习/main.py
@@ -0,0 +1,7 @@
+# 使用 print 打印：Hello World!
+print('Hello World!')
+# 使用 print 打印：I'm learning Python.
+print("I'm learning Python.")
+# 使用 print 打印，同时请用上 f-string：极创工作室开发了这个课程
+developer = '极创工作室'
+print(f'{developer}开发了这个课程')
---
+++ Python 入门/从 Hello World 开始/实践练习/task.md
@@ -0,0 +1,11 @@
+
+现在，是时候验证你对本次课程的理解了。
+
+请修改 [main.py 文件](main.py) 使用 print 输出以下内容：
+```text
+Hello World!
+I'm learning Python.
+极创工作室开发了这个课程
+```
+
+请注意，输出最后一行时，请使用 f-string，我们已经给定了变量：`developer`，请在文本中使用该变量
